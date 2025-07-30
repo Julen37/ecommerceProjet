cf34c429c1c2669d41ab7bc2ba9674e3074f248f
Julen37 <julen.gouchault@gmail.com>
2025-07-30 15:37:08 +0200

    exercice 4 role editor

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 #[Route('/admin')]
 final class UserController extends AbstractController
@@ -21,4 +24,18 @@ final class UserController extends AbstractController
             'users' => $userRepo->findAll(), // on met directement le find all ici au lieu de réécrire la ligne au dessus du return
         ]);
     }
+
+    // UPDATE ROLE - EDITOR / USER
+    #[Route('/user/role/update/{id}', name: 'app_user_update_role')] 
+    #[IsGranted("ROLE_ADMIN")]
+    public function updateRole(EntityManagerInterface $entityManager, User $user): Response
+    {
+        $user->setRoles(["ROLE_EDITOR", "ROLE_USER"]);
+        $entityManager->flush(); 
+
+        $this->addFlash('success', 'The user\'s role have been updated to Editor !');
+
+        return $this->redirectToRoute('app_user'); 
+    }
+    
 }
