bc8acc334893f590dbece9c259be9566e671c963
Julen37 <julen.gouchault@gmail.com>
2025-07-31 13:59:40 +0200

    protection route subcategory

--- src/Controller/SubCategoryController.php
+++ src/Controller/SubCategoryController.php
@@ -4,17 +4,20 @@ namespace App\Controller;
 
 use App\Entity\SubCategory;
 use App\Form\SubCategoryType;
-use App\Repository\SubCategoryRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\SubCategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 #[Route('/sub/category')]
 final class SubCategoryController extends AbstractController
 {
     #[Route(name: 'app_sub_category_index', methods: ['GET'])]
+    #[IsGranted("ROLE_ADMIN")]
     public function index(SubCategoryRepository $subCategoryRepository): Response
     {
         return $this->render('sub_category/index.html.twig', [
@@ -23,6 +26,7 @@ final class SubCategoryController extends AbstractController
     }
 
     #[Route('/new', name: 'app_sub_category_new', methods: ['GET', 'POST'])]
+    #[IsGranted("ROLE_ADMIN")]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $subCategory = new SubCategory();
@@ -43,6 +47,7 @@ final class SubCategoryController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_sub_category_show', methods: ['GET'])]
+    #[IsGranted("ROLE_ADMIN")]
     public function show(SubCategory $subCategory): Response
     {
         return $this->render('sub_category/show.html.twig', [
@@ -51,6 +56,7 @@ final class SubCategoryController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_sub_category_edit', methods: ['GET', 'POST'])]
+    #[IsGranted("ROLE_ADMIN")]
     public function edit(Request $request, SubCategory $subCategory, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(SubCategoryType::class, $subCategory);
@@ -69,6 +75,7 @@ final class SubCategoryController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_sub_category_delete', methods: ['POST'])]
+    #[IsGranted("ROLE_ADMIN")]
     public function delete(Request $request, SubCategory $subCategory, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$subCategory->getId(), $request->getPayload()->getString('_token'))) {
