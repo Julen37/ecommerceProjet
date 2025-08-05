93ac70c622181ff9cb38cd5e99adf663a3f18736
Julen37 <julen.gouchault@gmail.com>
2025-08-05 10:19:20 +0200

    update produit homepage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +12,25 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page', methods: ['GET'])]
-    public function homePage( ProductRepository $productRepo): Response
+    public function homePage(CategoryRepository $categoryRepo, ProductRepository $productRepo): Response
     {
+        $categories = $categoryRepo->findAll();
         $product = $productRepo->findAll();
 
         return $this->render('home_page/homePage.html.twig', [
             'controller_name' => 'HomePageController',
+            'categories'=> $categories,
             'products'=>$product
+        ]);
+    }
+
+    #[Route('/product/{id}/show', name: 'app_home_product_show', methods: ['GET'])]
+    public function showProduct(Product $product): Response
+    {
+
+        return $this->render('home_page/show.html.twig', [
+            'controller_name' => 'HomePageController',
+            'product'=>$product
         ]);
     }
 }
