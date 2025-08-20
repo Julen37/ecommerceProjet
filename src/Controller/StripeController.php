e5edcfdb7e332c051f6b9c974b5433381d3d36d1
Julen37 <julen.gouchault@gmail.com>
2025-08-20 09:21:48 +0200

    update stripe

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Stripe\Stripe;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class StripeController extends AbstractController
 {
@@ -22,5 +24,44 @@ final class StripeController extends AbstractController
         return $this->render('stripe/cancel.html.twig', [
             'controller_name' => 'StripeController',
         ]);
+    }
+
+    #[Route('/stripe/notify', name: 'app_stripe_notify')]
+    public function stripeNotify(Request $request): Response
+    {
+        Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY']);
+        file_put_contents("log.txt", ""); 
+        $endpoint_secret = $_SERVER['STRIPE_SECRET_KEY_WEBHOOK'];
+        $payload = $request->getContent();
+        file_put_contents("log.txt", $payload, FILE_APPEND);
+        $sigHeader = $request->headers->get('Stripe-Signature');
+        $event = null;
+
+        try {
+            $event = \Stripe\Webhook::constructEvent(
+                $payload, $sigHeader, $endpoint_secret
+            );
+        file_put_contents("log.txt", "try ok", FILE_APPEND);
+        } catch(\UnexpectedValueException $e){
+            return new Response('Invalid payload', 400);
+        } catch(\Stripe\Exception\SignatureVerificationException $e){
+            return new Response('Invalid signature', 400);
+        }
+
+        switch($event->type){
+            case 'payment_intent.succeeded':
+                $paymentIntent = $event->data->object;
+
+                $fileName = 'stripe-detail-'.uniqid().'.txt';
+                file_put_contents($fileName, $paymentIntent);
+                break;
+            case 'payment_method.attached':
+                $paymentMethod =$event->data->object;
+                break;
+            default:
+                break;
+        }
+        
+        return new Response('Event success', 200);
     }
 }
