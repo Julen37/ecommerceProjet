2f7fbe62eb62118efcd5efb07897cde025df05ad
Julen37 <julen.gouchault@gmail.com>
2025-08-21 15:53:45 +0200

    searchEngine update

---
+++ src/Controller/SearchEngineController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SearchEngineController extends AbstractController
+{
+    #[Route('/search/engine', name: 'app_search_engine')]
+    public function index(Request $request, ProductRepository $productRepo): Response
+    {
+
+        if ($request->isMethod('POST')) {
+            // $data = $request->request->all();
+            // $word = $data['word'];
+            $word = $request->get('word');
+
+            $results = $productRepo->searchEngine($word);
+        }
+
+        return $this->render('search_engine/index.html.twig', [
+            'controller_name' => 'SearchEngineController',
+            'products' => $results,
+            'word'=> $word,
+        ]);
+    }
+}
